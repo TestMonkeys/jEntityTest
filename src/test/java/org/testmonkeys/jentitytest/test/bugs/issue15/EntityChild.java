4fd0563087562a4dae8404f8bd837ca02bab6944
Constantin Pascal <costea32@gmail.com>
2016-11-05 16:59:02 +0200

    Fixing bug #15

---
+++ src/test/java/org/testmonkeys/jentitytest/test/bugs/issue15/EntityChild.java
@@ -0,0 +1,38 @@
+package org.testmonkeys.jentitytest.test.bugs.issue15;
+
+import org.testmonkeys.jentitytest.framework.ChildEntityComparison;
+
+public class EntityChild {
+
+    @ChildEntityComparison
+    private EntityParent mom;
+
+    @ChildEntityComparison
+    private EntityParent dad;
+
+    private int age;
+
+    public EntityParent getDad() {
+        return dad;
+    }
+
+    public void setDad(EntityParent dad) {
+        this.dad = dad;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public EntityParent getMom() {
+        return mom;
+    }
+
+    public void setMom(EntityParent mom) {
+        this.mom = mom;
+    }
+}
