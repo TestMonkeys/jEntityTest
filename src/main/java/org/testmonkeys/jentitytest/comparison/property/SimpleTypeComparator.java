3dd5dc609a6060c41cb4d7acf0a39e32cc15b420
Constantin Pascal <costea32@gmail.com>
2016-11-12 00:22:47 +0200

    Refactored comparator classes to contain less parameters as they are not used. Removed FailedComparisonResult class

--- src/main/java/org/testmonkeys/jentitytest/comparison/property/SimpleTypeComparator.java
+++ src/main/java/org/testmonkeys/jentitytest/comparison/property/SimpleTypeComparator.java
@@ -6,12 +6,10 @@ import org.testmonkeys.jentitytest.comparison.result.ComparisonResult;
 import org.testmonkeys.jentitytest.comparison.util.NullComparator;
 import org.testmonkeys.jentitytest.comparison.util.NullComparisonResult;
 
-import java.beans.PropertyDescriptor;
-
 public class SimpleTypeComparator extends SingleResultComparator {
     private final NullComparator nullComparatorHelper = new NullComparator();
     @Override
-    public ComparisonResult computeSingleComparison(PropertyDescriptor property, Object actualValue, Object expectedValue, ComparisonContext context) {
+    public ComparisonResult computeSingleComparison(Object actualValue, Object expectedValue, ComparisonContext context) {
         ComparisonResult result = new ComparisonResult(false, context, actualValue, expectedValue);
         NullComparisonResult nullComparisonResult = nullComparatorHelper.compareOnNulls(actualValue, expectedValue, context);
         if (!nullComparisonResult.isPassed() || nullComparisonResult.stopComparison())
