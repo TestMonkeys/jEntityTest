c647c1909a87368d1d24272b612060dfd9ccf2ed
Maria Burunducova <mburunducova@gmail.com>
2016-11-11 17:18:49 +0200

    fixed issue #38

--- src/main/java/org/testmonkeys/jentitytest/comparison/property/SimpleTypeComparator.java
+++ src/main/java/org/testmonkeys/jentitytest/comparison/property/SimpleTypeComparator.java
@@ -3,14 +3,19 @@ package org.testmonkeys.jentitytest.comparison.property;
 import org.testmonkeys.jentitytest.comparison.ComparisonContext;
 import org.testmonkeys.jentitytest.comparison.SingleResultComparator;
 import org.testmonkeys.jentitytest.comparison.result.ComparisonResult;
+import org.testmonkeys.jentitytest.comparison.util.NullComparator;
+import org.testmonkeys.jentitytest.comparison.util.NullComparisonResult;
 
 import java.beans.PropertyDescriptor;
 
 public class SimpleTypeComparator extends SingleResultComparator {
-
+    private final NullComparator nullComparatorHelper = new NullComparator();
     @Override
     public ComparisonResult computeSingleComparison(PropertyDescriptor property, Object actualValue, Object expectedValue, ComparisonContext context) {
         ComparisonResult result = new ComparisonResult(false, context, actualValue, expectedValue);
+        NullComparisonResult nullComparisonResult = nullComparatorHelper.compareOnNulls(actualValue, expectedValue, context);
+        if (!nullComparisonResult.isPassed() || nullComparisonResult.stopComparison())
+            return nullComparisonResult;
         result.setPassed(actualValue.equals(expectedValue));
 
         return result;
