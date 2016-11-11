3900811b19b23c2883e100be100351b53163778a
Constantin Pascal <costea32@gmail.com>
2016-11-12 00:22:47 +0200

    Refactored comparator classes to contain less parameters as they are not used. Removed FailedComparisonResult class

--- src/main/java/org/testmonkeys/jentitytest/comparison/SingleResultComparator.java
+++ src/main/java/org/testmonkeys/jentitytest/comparison/SingleResultComparator.java
@@ -2,19 +2,18 @@ package org.testmonkeys.jentitytest.comparison;
 
 import org.testmonkeys.jentitytest.comparison.result.ComparisonResult;
 
-import java.beans.PropertyDescriptor;
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class SingleResultComparator extends MultiResultComparator {
 
     @Override
-    protected List<ComparisonResult> computeComparison(PropertyDescriptor property, Object actualValue, Object expectedValue, ComparisonContext context) {
+    protected List<ComparisonResult> computeComparison(Object actualValue, Object expectedValue, ComparisonContext context) {
         List<ComparisonResult> result = new ArrayList<>();
-        result.add(computeSingleComparison(property, actualValue, expectedValue, context));
+        result.add(computeSingleComparison(actualValue, expectedValue, context));
         return result;
     }
 
 
-    protected abstract ComparisonResult computeSingleComparison(PropertyDescriptor property, Object actualValue, Object expectedValue, ComparisonContext context);
+    protected abstract ComparisonResult computeSingleComparison(Object actualValue, Object expectedValue, ComparisonContext context);
 }
--- src/test/java/org/testmonkeys/jentitytest/test/unit/entityInspectionTests/models/IgnoreComparatorCustom.java
+++ src/test/java/org/testmonkeys/jentitytest/test/unit/entityInspectionTests/models/IgnoreComparatorCustom.java
@@ -3,21 +3,12 @@ package org.testmonkeys.jentitytest.test.unit.entityInspectionTests.models;
 import org.testmonkeys.jentitytest.comparison.ComparisonContext;
 import org.testmonkeys.jentitytest.comparison.SingleResultComparator;
 import org.testmonkeys.jentitytest.comparison.result.ComparisonResult;
-
-import java.beans.PropertyDescriptor;
+import org.testmonkeys.jentitytest.comparison.result.PassedComparisonResult;
 
 public class IgnoreComparatorCustom extends SingleResultComparator {
 
     @Override
-    protected ComparisonResult computeSingleComparison(PropertyDescriptor property, Object actual, Object expected, ComparisonContext context) {
-        ComparisonResult result = new ComparisonResult();
-        result.setPassed(true);
-        Object actualValue = getPropertyValue(property, actual);
-        Object expectedValue = getPropertyValue(property, expected);
-
-        result.setComparisonContext(context);
-        result.setActual(actualValue);
-        result.setExpected(expectedValue);
-        return result;
+    protected ComparisonResult computeSingleComparison(Object actualValue, Object expectedValue, ComparisonContext context) {
+        return new PassedComparisonResult(context, actualValue, expectedValue);
     }
 }
