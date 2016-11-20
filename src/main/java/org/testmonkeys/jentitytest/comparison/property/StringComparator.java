969379329269d97536db67bca4459efa6d599dd9
cat-bug <mburunducova@gmail.com>
2016-11-20 16:58:13 +0200

    fixed using nullConditional check instead of nullComparator

--- src/main/java/org/testmonkeys/jentitytest/comparison/property/StringComparator.java
+++ src/main/java/org/testmonkeys/jentitytest/comparison/property/StringComparator.java
@@ -2,24 +2,20 @@ package org.testmonkeys.jentitytest.comparison.property;
 
 import org.testmonkeys.jentitytest.comparison.ComparisonContext;
 import org.testmonkeys.jentitytest.comparison.SingleResultComparator;
+import org.testmonkeys.jentitytest.comparison.conditionalChecks.NullConditionalCheck;
 import org.testmonkeys.jentitytest.comparison.result.ComparisonResult;
-import org.testmonkeys.jentitytest.comparison.util.NullComparator;
-import org.testmonkeys.jentitytest.comparison.util.NullComparisonResult;
 import org.testmonkeys.jentitytest.framework.JEntityTestException;
 
 public class StringComparator extends SingleResultComparator {
-    private final NullComparator nullComparatorHelper = new NullComparator();
     private boolean caseSensitive = true;
     private char[] ignoreCharacters = {};
 
+    public StringComparator() {
+        registerPreConditionalCheck(new NullConditionalCheck());
+    }
+
     public ComparisonResult computeSingleComparison(Object actualValue, Object expectedValue, ComparisonContext context) {
         ComparisonResult comparisonResult = new ComparisonResult(false, context, actualValue, expectedValue);
-
-        NullComparisonResult nullComparisonResult = nullComparatorHelper.compareOnNulls(actualValue, expectedValue,
-                context);
-        if (!nullComparisonResult.isPassed() || nullComparisonResult.stopComparison()) {
-            return nullComparisonResult;
-        }
 
         String actual;
         String expected;
@@ -32,11 +28,14 @@ public class StringComparator extends SingleResultComparator {
                     castException);
         }
 
-        for (char ignore : ignoreCharacters) {
-            String ignoreString = String.valueOf(ignore);
-            actual = actual.replaceAll(ignoreString, "");
-            expected = expected.replaceAll(ignoreString, "");
+        if (actual != null && expected != null) {
+            for (char ignore : ignoreCharacters) {
+                String ignoreString = String.valueOf(ignore);
+                actual = actual.replaceAll(ignoreString, "");
+                expected = expected.replaceAll(ignoreString, "");
+            }
         }
+
 
         if (caseSensitive) {
             comparisonResult.setPassed(actual.equals(expected));
