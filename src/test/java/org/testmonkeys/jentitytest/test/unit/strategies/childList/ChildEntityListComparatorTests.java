7107611a61f56d1d928d57187eefe0a1ab90dfbf
Constantin Pascal <costea32@gmail.com>
2016-11-26 15:49:51 +0200

    Draft implementation of ChildEntityComparator

--- src/test/java/org/testmonkeys/jentitytest/test/unit/strategies/childList/ChildEntityListComparatorTests.java
+++ src/test/java/org/testmonkeys/jentitytest/test/unit/strategies/childList/ChildEntityListComparatorTests.java
@@ -1,36 +1,87 @@
 package org.testmonkeys.jentitytest.test.unit.strategies.childList;
 
+import org.hamcrest.junit.ExpectedException;
+import org.junit.Rule;
 import org.junit.Test;
+import org.testmonkeys.jentitytest.comparison.ComparisonContext;
 import org.testmonkeys.jentitytest.comparison.entity.ChildEntityListComparator;
-import org.testmonkeys.jentitytest.matchers.ObjectPropertyComparator;
+import org.testmonkeys.jentitytest.comparison.result.ComparisonResult;
+import org.testmonkeys.jentitytest.framework.JEntityTestException;
 
-import java.beans.IntrospectionException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.Assert.assertTrue;
+
 public class ChildEntityListComparatorTests {
 
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
     @Test
-    public void unit_ChildEntity() throws IntrospectionException {
-        ObjectPropertyComparator comparator = new ObjectPropertyComparator(new ChildEntityListComparator());
-        TestModel expected = new TestModel();
-        expected.setListItem(new ArrayList<>());
-        TestModel actual = new TestModel();
-        actual.setListItem(new ArrayList<>());
-        //comparator.areEqual( actual, expected, new ComparisonContext());
+    public void actualNotCollection() {
+        expectedException.expect(JEntityTestException.class);
+        expectedException.expectMessage("Actual and Expected should be Generic Collections");
+        ChildEntityListComparator comparator = new ChildEntityListComparator();
+        comparator.areEqual("a string", new ArrayList<String>(), new ComparisonContext());
     }
 
-    public class TestModel {
-
-        private List<String> listItem;
-
-
-        public List<String> getListItem() {
-            return listItem;
-        }
-
-        public void setListItem(List<String> listItem) {
-            this.listItem = listItem;
-        }
+    @Test
+    public void expectedNotCollection() {
+        expectedException.expect(JEntityTestException.class);
+        expectedException.expectMessage("Actual and Expected should be Generic Collections");
+        ChildEntityListComparator comparator = new ChildEntityListComparator();
+        comparator.areEqual(new ArrayList<String>(), "a string", new ComparisonContext());
     }
+
+    @Test
+    public void emptyCollections() {
+        ChildEntityListComparator comparator = new ChildEntityListComparator();
+        List<ComparisonResult> results = comparator.areEqual(new ArrayList<String>(), new ArrayList<String>(), new ComparisonContext());
+
+        assertTrue(results.stream().allMatch(ComparisonResult::isPassed));
+    }
+
+    @Test
+    public void stringCollections_ValueMismatch() {
+        ChildEntityListComparator comparator = new ChildEntityListComparator();
+        List<String> actual = new ArrayList<>();
+        List<String> expected = new ArrayList<>();
+        actual.add("Line1Actual");
+        expected.add("Line1Expected");
+
+        List<ComparisonResult> results = comparator.areEqual(actual, expected, new ComparisonContext());
+
+        assertTrue(results.stream().anyMatch(res -> !res.isPassed()));
+    }
+
+    @Test
+    public void stringCollections_CountMismatch() {
+        ChildEntityListComparator comparator = new ChildEntityListComparator();
+        List<String> actual = new ArrayList<>();
+        List<String> expected = new ArrayList<>();
+        actual.add("Line1Actual");
+        actual.add("Line2Actual");
+        expected.add("Line1Actual");
+
+        List<ComparisonResult> results = comparator.areEqual(actual, expected, new ComparisonContext());
+
+        assertTrue(results.stream().anyMatch(res -> !res.isPassed()));
+    }
+
+    @Test
+    public void stringCollections_Match() {
+        ChildEntityListComparator comparator = new ChildEntityListComparator();
+        List<String> actual = new ArrayList<>();
+        List<String> expected = new ArrayList<>();
+        actual.add("Line1Actual");
+        actual.add("Line2Actual");
+        expected.add("Line1Actual");
+        expected.add("Line2Actual");
+
+        List<ComparisonResult> results = comparator.areEqual(actual, expected, new ComparisonContext());
+
+        assertTrue(results.stream().allMatch(ComparisonResult::isPassed));
+    }
+
 }
