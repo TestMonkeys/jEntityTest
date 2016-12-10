8aec3b6251bde77f88bc592c119e765439b6b7c2
Constantin Pascal <costea32@gmail.com>
2016-12-10 12:36:17 +0200

    Issue #56 Splitting into two modules, creating specific module for Hamcrest

--- src/main/java/org/testmonkeys/jentitytest/comparison/ConditionalCheck.java
+++
@@ -1,8 +0,0 @@
-package org.testmonkeys.jentitytest.comparison;
-
-import org.testmonkeys.jentitytest.comparison.result.ConditionalCheckResult;
-
-public interface ConditionalCheck {
-
-    ConditionalCheckResult runCheck(Object actual, Object expected, ComparisonContext context);
-}
