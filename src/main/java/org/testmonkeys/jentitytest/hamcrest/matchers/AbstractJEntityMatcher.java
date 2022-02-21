89e061b3b927d2994707412ffc2620cfa07d0574
Constantin Pascal <constantin.pascal@testmonkeys.org>
2022-02-21 22:48:26 +0200

    Reducing code duplication for output description generation

--- src/main/java/org/testmonkeys/jentitytest/hamcrest/matchers/AbstractJEntityMatcher.java
+++ src/main/java/org/testmonkeys/jentitytest/hamcrest/matchers/AbstractJEntityMatcher.java
@@ -1,6 +1,13 @@
 package org.testmonkeys.jentitytest.hamcrest.matchers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.BaseMatcher;
+import org.testmonkeys.jentitytest.Resources;
+
+import java.text.MessageFormat;
+
+import static org.testmonkeys.jentitytest.Resources.desc_entities_same;
 
 
 @SuppressWarnings("WeakerAccess")
@@ -13,5 +20,14 @@ public abstract class AbstractJEntityMatcher<T> extends BaseMatcher<T> {
 
     public void setResultProcessor(ResultProcessor resultProcessor) {
         this.resultProcessor = resultProcessor;
+    }
+
+    protected String generateDescriptionFor(String resourceMessage, Object expected) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return MessageFormat.format(Resources.getString(resourceMessage), objectMapper.writeValueAsString(expected));
+        } catch (JsonProcessingException e) {
+            return MessageFormat.format(Resources.getString(desc_entities_same), expected);
+        }
     }
 }
