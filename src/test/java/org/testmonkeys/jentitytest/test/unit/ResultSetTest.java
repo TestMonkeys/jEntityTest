81328b1ce8c0a273c14bc903c26f8417dc6dc1eb
Constantin Pascal <constantin.pascal@testmonkeys.org>
2022-02-21 19:57:46 +0200

    Adding result set unit tests

--- src/test/java/org/testmonkeys/jentitytest/test/unit/ResultSetTest.java
+++ src/test/java/org/testmonkeys/jentitytest/test/unit/ResultSetTest.java
@@ -6,6 +6,9 @@ import org.testmonkeys.jentitytest.comparison.result.ComparisonResult;
 import org.testmonkeys.jentitytest.comparison.result.ResultSet;
 import org.testmonkeys.jentitytest.comparison.result.Status;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ResultSetTest {
 
     @Test
@@ -23,5 +26,29 @@ public class ResultSetTest {
         result.add(new ComparisonResult(Status.Skipped, null, null, null));
         result.add(new ComparisonResult(Status.Failed, null, null, null));
         Assert.assertFalse("perfect match", result.isPerfectMatch());
+    }
+
+    @Test
+    public void getMismatches_none() {
+        ResultSet result = new ResultSet();
+        result.add(new ComparisonResult(Status.Passed, null, null, null));
+        result.add(new ComparisonResult(Status.Skipped, null, null, null));
+        List<ComparisonResult> comparisonResultList = new ArrayList<>();
+        for (ComparisonResult item : result.getMismatches())
+            comparisonResultList.add(item);
+        Assert.assertEquals("mismatches", comparisonResultList.size(), 0);
+    }
+
+    @Test
+    public void getMismatches_one() {
+        ResultSet result = new ResultSet();
+        result.add(new ComparisonResult(Status.Passed, null, null, null));
+        result.add(new ComparisonResult(Status.Skipped, null, null, null));
+        result.add(new ComparisonResult(Status.Failed, null, null, null));
+        List<ComparisonResult> comparisonResultList = new ArrayList<>();
+        for (ComparisonResult item : result.getMismatches())
+            comparisonResultList.add(item);
+        Assert.assertEquals("mismatches", comparisonResultList.size(), 1);
+        Assert.assertEquals(comparisonResultList.get(0).getStatus(), Status.Failed);
     }
 }
